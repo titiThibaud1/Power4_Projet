ac7a4d8695b59a9d3d9587876423f80591884dbf
ThibaudS <thibaud.sellier@ynov.com>
2025-10-13 15:40:12 +0200

    Regroupement du code go dans un seul fichier pour une facilité a dev + création du choix des joueurs + choix de la difficulté + Affichage de la grille en fonction de la difficulté en caractère

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"net/http"
 	power4 "power4/src"
 )
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	power4.Server()
 }
--- src/server.go
+++ src/server.go
@@ -2,17 +2,131 @@ package power4
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type InfosGame struct {
+	Player1    string
+	Player2    string
+	Difficulty string
+	Board      [][]int
+}
+
 func Server() {
+	var infosGameVar InfosGame
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/static"))))
+
 	http.HandleFunc("/", namescreenHandler)
-	http.HandleFunc("/difficulty", difficultyHandler)
-	http.HandleFunc("/play", playHandler)
+	http.HandleFunc("/difficulty", func(w http.ResponseWriter, r *http.Request) {
+		difficultyHandler(w, r, &infosGameVar)
+	})
+	http.HandleFunc("/play", func(w http.ResponseWriter, r *http.Request) {
+		playHandler(w, r, &infosGameVar)
+	})
 	http.HandleFunc("/win", winHandler)
 	http.HandleFunc("/draw", drawHandler)
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+// ======================================================================================
+
+// Route /
+func namescreenHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpl.Execute(w, nil)
+}
+
+// Route /difficulty
+func difficultyHandler(w http.ResponseWriter, r *http.Request, infosGameVar *InfosGame) {
+	infosGameVar = &InfosGame{
+		Player1: r.FormValue("Player1"),
+		Player2: r.FormValue("Player2"),
+	}
+
+	tmpl, err := template.ParseFiles("pages/difficultyscreen.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpl.Execute(w, infosGameVar)
+}
+
+// Route /play
+func playHandler(w http.ResponseWriter, r *http.Request, infosGameVar *InfosGame) {
+	infosGameVar = &InfosGame{
+		Difficulty: r.FormValue("difficulty"),
+	}
+	switch infosGameVar.Difficulty {
+	case "Easy":
+		infosGameVar.Board = [][]int{
+			{1, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+		}
+	case "Medium":
+		infosGameVar.Board = [][]int{
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},
+		}
+	case "Hard":
+		infosGameVar.Board = [][]int{
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+		}
+	}
+
+	tmpl, err := template.ParseFiles("pages/playscreen.html")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpl.Execute(w, infosGameVar)
+}
+
+// Route /win
+func winHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Route /draw
+func drawHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// ==================================================================================
+
+func NewGame() {
+	// var Hor et Ver
+}
+
+func gamePower4() {
+
+}
+
+//board[1][2] = 42
